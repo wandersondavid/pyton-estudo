24225d162a547d661c677c514094ea9a89b300f7
Wanderson David <wandersondavid.ifg@gmail.com>
2022-05-23 16:53:23 -0300

    estuso em pyton para Data science

---
+++ jogos/adivinhacao.py
@@ -0,0 +1,19 @@
+print('-----------------------------')
+print('Bem vindo no jogo Adivinhação')
+print('-----------------------------')
+
+
+numero_secreto = 74
+numero_secreto1 = 74
+
+print('Você digitou', chute)
+
+
+
+chute = input('Digite o seu numero: ')
+print('Você digitou', chute)
+
+if (numero_secreto == int(chute)):
+    print('Você acertou')
+else:
+    print('Você errouu')
